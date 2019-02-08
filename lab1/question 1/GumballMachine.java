cff74a400db5442130713a24b0e205cca23ce280
Kaustubh Hassan Narasimhan <kaustubh.hassannarasimhan@sjsu.edu>
2019-02-07 17:22:57 -0800

    Lab 1 Question 1

---
+++ lab1/question 1/GumballMachine.java
@@ -0,0 +1,14 @@
+package org.launchcode;
+
+public class GumballMachineMain {
+    protected boolean has_quarter1;
+    protected boolean has_quarter2;
+
+    public GumballMachineMain()
+    {
+        // initialise instance variables
+        this.has_quarter1 = false;
+        this.has_quarter2 = false;
+    }
+
+}
---
+++ lab1/question 1/Main.java
@@ -0,0 +1,20 @@
+package org.launchcode;
+
+public class Main {
+
+    public static void main(String[] args) {
+       GumballMachine3 gumball = new GumballMachine3(5);
+
+       gumball.insertCoins(5);
+        gumball.insertCoins(5);
+        gumball.insertCoins(5);
+        gumball.insertCoins(10);
+        gumball.insertCoins(25);
+
+        gumball.turnCrank();
+
+        GumballMachine2 gumball2 = new GumballMachine2(5);
+        gumball2.insertTwoQuarters(25,20);
+        gumball2.turnCrank();
+    }
+}
